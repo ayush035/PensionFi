67bfb6a39238b475ca7895bfcfbacee02ac8af7e
holyaustin <holyaustin@yahoo.com>
2022-05-19 09:44:28 +0100

    UI commit

--- client/vite.config.js
+++ client/vite.config.js
@@ -1,24 +1,7 @@
-/* eslint-disable import/no-extraneous-dependencies */
-import { resolve } from "path";
-import { defineConfig } from "vite";
-import react from "@vitejs/plugin-react";
+import { defineConfig } from 'vite'
+import react from '@vitejs/plugin-react'
 
-const outDir = resolve(__dirname, "dist");
-// const root = resolve(__dirname, "src");
 // https://vitejs.dev/config/
 export default defineConfig({
-  plugins: [react()],
-  build: {
-    outDir,
-    emptyOutDir: true,
-    rollupOptions: {
-      input: {
-        // main: resolve(root, "index.html"),
-        // create: resolve(root, "create", "index.html"),
-        // main: resolve(__dirname, "src/index.html"),
-        // create: resolve(__dirname, "create/index.html")
-      }
-    }
-  }
-});
-
+  plugins: [react()]
+})
